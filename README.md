ef4a5540745886dc1b7fee33b84afb03fa5f0b65
redfast00 <redfast00@gmail.com>
2023-09-14 16:39:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Single-keyboard, multi-player Flash games; now over the network!
+
+Remember, way back, when Flash games were all the hype, and you had a friend come over to play a game together? You'd share the same keyboard, with one of you controlling a character with the arrow keys, and the other controlling their character with the WASD keys. Now, thanks to [Ruffle](https://ruffle.rs/) (a Flash emulator) and modern browser technology, you can play those games over the internet!
+
+## How does it work?
+
+One player, Alice, is the host: she will have the game running in Ruffle, and will stream a videostream of the game to Bob (the guest). Bob will send his keyboard events to Alice; these are then injected back into the game. Thanks to WebRTC, this can be entirely done in a peer-to-peer fashion: we don't need a game server. This demo runs entirely in the browser.
+
+## Future features
+
+At the moment, the game is playable, but some features aren't implemented yet.
+
+- [ ] The layout looks pretty bad; I'm not a frontend developer
+- [ ] Selecting games: at the moment, "Tank Trouble" is hardcoded, but this can be swapped out easily.
+- [ ] Lobby & matchmaking (we'll need a small backend for this)
+    - At the moment, the guest must send their ID out-of-band to the host
+    - A lobby system could be implemented, where guests send their ID to a backend server, and hosts request a guest ID
+    - Risk with this approach: this lobby-server approach would make it easy for an attacker to obtain guest IDs, with which they could stream arbitrary video to guests.
+- [ ] Optimize video for latency
+- [ ] Compensate lag: at the moment, the host has an advantage over the host with regards to latency: they immediately see the game, and their input arrives faster. To make it fair, the host view and input can be artificially delayed, until the guest and host have the same video and input latency.
+- [ ] Sound: the game sound is not transmitted to the guest yet
---
+++ main.js
@@ -0,0 +1,138 @@
+"use strict";
+
+let game_keyup = null;
+let game_keydown = null;
+
+
+window.RufflePlayer = window.RufflePlayer || {};
+
+var callIntervalId = null;
+
+var guest_video_id = null;
+var guest_data_id = null;
+
+function on_host_load() {
+    const ruffle = window.RufflePlayer.newest();
+    const player = ruffle.createPlayer();
+    const container = document.getElementById("container");
+    player.style.width = "100%";
+    player.style.height = "800px";
+    container.appendChild(player);
+    player.load("tank-trouble.swf");
+    const peer = new Peer();
+    console.log("peer=", peer);
+    peer.on('open', function(id) {
+        console.log('My peer ID is: ' + id);
+        let conn = peer.connect(guest_data_id);
+        conn.on('open', function() {
+            console.log("Keyboard connection established");
+            // Receive messages
+            conn.on('data', function(data) {
+                console.log("received data", data);
+                window.dispatchEvent(new KeyboardEvent(data["type"], {
+                    code: data['code'],
+                }));
+            });
+        });
+    });
+        
+
+    const videopeer = new Peer();
+    callIntervalId = setInterval(function(p) {
+        const canvasElt = document.querySelector("ruffle-player")?.shadowRoot.querySelector("canvas");
+        if (canvasElt != null) {
+            console.log("Canvas exists, setting up call now");
+            const stream = canvasElt.captureStream(25); // 25 FPS
+            var call = p.call(guest_video_id, stream);
+            console.log("stream=", stream);
+            // Disabled, we'll re-enable this for lag-compensation
+            // document.getElementById("receiving-video").srcObject = stream;
+            // document.getElementById("receiving-video").play();
+            clearInterval(callIntervalId);
+        } else {
+            console.log("canvas still null");
+        }
+    }, 1000, videopeer);
+}
+
+function transmitKeystroke(conn, type, event) {
+    console.log("transmitting ", type, event);
+    conn.send({type: type, code: event.code});
+}
+
+var displayPeerIdIntervalId = null;
+
+function on_guest_load() {
+    const peer = new Peer();
+    
+    console.log("peer=", peer);
+    peer.on('open', function(id) {
+        console.log('Opened, data peer ID is: ' + id);
+        guest_data_id = id;
+    });
+    peer.on('connection', function(conn) {
+        document.getElementById("connectiondetails").innerHTML = "";
+        conn.on('open', function() {
+            console.log("Keyboard connection established");
+            document.addEventListener("keyup", function(ev) {transmitKeystroke(conn, "keyup", ev)});
+            document.addEventListener("keydown", function(ev) {transmitKeystroke(conn, "keydown", ev)});
+        });
+    });
+    
+
+    const videopeer = new Peer();
+    videopeer.on('open', function(id) {
+        console.log('Opened, video peer ID is: ' + id);
+        guest_video_id = id;
+    })
+    videopeer.on('call', function(call) {
+        console.log("received call");
+        call.on('stream', function(stream) {
+            console.log("On stream, setting video element to ", stream);
+            document.getElementById("receiving-video").srcObject = stream;
+            document.getElementById("receiving-video").play();
+        });
+        call.answer();
+    });
+
+    displayPeerIdIntervalId = setInterval(function() {
+        if (guest_data_id != null && guest_video_id != null) {
+            let combinedID = `${guest_data_id}/${guest_video_id}`
+            document.getElementById("connectiondetails").innerHTML = 
+                `Please pass your connection ID <input readonly size="${combinedID.length}" value="${combinedID}"></input> to the host.
+                The game will automatically start when the host clicks the 'Start game' button`
+            clearInterval(displayPeerIdIntervalId);
+        } else {
+            console.log("still null");
+        }
+    }, 200);
+
+
+}
+
+function submit_host_id() {
+    let guest_combined_id = document.getElementById("guest_combined_id").value.trim();
+    if (guest_combined_id.length == 73) {
+        guest_data_id = guest_combined_id.split('/')[0];
+        guest_video_id = guest_combined_id.split('/')[1];
+        on_host_load();
+        document.getElementById("connectiondetails").innerHTML = '';
+    } else {
+        document.getElementById("error-connectiondetails").innerText = "That does not look right";
+    }
+}
+
+function click_host() {
+    document.getElementById("hostguestchoice").remove();
+    document.getElementById("connectiondetails").innerHTML = `
+        <p>Please paste the ID you received from the guest</p>
+        <input id="guest_combined_id" size="73">
+        <button onclick="submit_host_id()">Start game</button>
+        <div id="error-connectiondetails"></div>
+    `
+}
+
+function click_guest() {
+    document.getElementById("hostguestchoice").remove();
+    on_guest_load();
+}
